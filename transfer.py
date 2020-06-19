a688cd1a16c03df3d7b909313500f0474f6b893a
Ulises-Rosas <ulisesfrosasp@gmail.com>
2020-06-18 23:55:38 -0500

    adding a help page

--- transfer.py
+++ transfer.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+
 # passwordless entrance to clusters:
 # $ ssh-keygen # (leave it without password)
 # $ ssh-copy-id -i /Users/admin/.ssh/id_rsa ulisesrosasp@login.colonialone.gwu.edu
@@ -7,29 +10,80 @@
 import os
 import sys
 import shutil
+import argparse
 import subprocess
 
-frc = "scp -q -o LogLevel=QUIET -i /Users/admin/.ssh/id_rsa         -r ulisesrosasp@login.colonialone.gwu.edu:/lustre/groups/ortilab/bb1/target/{d} ."
-toc = "scp -q -o LogLevel=QUIET -i /Users/admin/.ssh/id_rsa_pegasus -r {d} ulisesrosasp@pegasus.colonialone.gwu.edu:/lustre/groups/ortilab/protacanthopterygii/target"
+# handy default files
+# COLONIAL_PATH = '/lustre/groups/ortilab/bb1/target'
+# PEGASUS_PATH  = '/lustre/groups/ortilab/protacanthopterygii/target'
 
-# dirs = [
-#     "Myctophidae_Lampichthys_procerus_EPLATE_49_H06",
-#     "Mormyridae_Marcusenius_sanagaensis_EPLATE_58_A05"
-# ]
-
-dirs = open("target_dirs.txt", "r").readlines()
-
-for d in dirs:
-    tmp  = d.strip()
-    # down
-    sys.stdout.write("\n    ,   : %s\r" % tmp)
-    downd = frc.format(d = tmp).split()
-    subprocess.Popen(downd).communicate()
-    sys.stdout.write( "Down,   : %s\r" % tmp)
-    # up
-    upd   = toc.format(d = tmp).split()
-    subprocess.Popen(upd).communicate()
-    sys.stdout.write( "Down, Up: %s" % tmp)
-    shutil.rmtree(tmp)
+def getOpts():
     
-sys.stdout.write("\n\n")
+    parser = argparse.ArgumentParser( 
+        formatter_class = argparse.RawDescriptionHelpFormatter, 
+        description = '''
+                        Transfer of directories from 
+                        colonialone to pegasus
+                        using keys
+                        ''')
+    parser.add_argument('-f', '--file',
+                        metavar  = "",
+                        type     = str,
+                        required = True,
+                        help     = '[Optional] File with list of directories [Default = None]')
+    parser.add_argument('-k1', '--colonial_key',
+                        metavar  = "",
+                        type     = str,
+                        required = True,
+                        help     = '[Optional] Colonial ssh key [Default = None]')
+    parser.add_argument('-p1', '--colonial_path',
+                        metavar  = "",
+                        type     = str,
+                        required = True,
+                        help     = '[Optional] Path where files are at colonial one [Default = None]')
+    parser.add_argument('-k2', '--pegasus_key',
+                        metavar  = "",
+                        type     = str,
+                        required = True,
+                        help     = '[Optional] Pegasus ssh key [Default = None]')
+    parser.add_argument('-p2', '--pegasus_path',
+                        metavar  = "",
+                        type     = str,
+                        required = True,
+                        help     = '[Optional] Path where files are at colonial one [Default = None]')
+    return parser.parse_args()
+
+def main():  
+    args = getOpts()
+    
+    frc = "scp -q -o LogLevel=QUIET -i {k1} -r ulisesrosasp@login.colonialone.gwu.edu:{p1}/{d} ."
+    toc = "scp -q -o LogLevel=QUIET -i {k2} -r {d} ulisesrosasp@pegasus.colonialone.gwu.edu:{p2}"
+    # dirs = [
+    #     "Myctophidae_Lampichthys_procerus_EPLATE_49_H06",
+    #     "Mormyridae_Marcusenius_sanagaensis_EPLATE_58_A05"
+    # ]
+    dirs = open(args.file, "r").readlines()
+
+    for d in dirs:
+        tmp  = d.strip()
+        
+        # down
+        sys.stdout.write("\n    ,   : %s\r" % tmp)
+        downd = frc.format(k1 = args.colonial_key,
+                           d  = tmp,
+                           p1 = args.colonial_path).split()
+        subprocess.Popen(downd).communicate()
+        sys.stdout.write( "Down,   : %s\r" % tmp)
+        
+        # up
+        upd   = toc.format(k2 = args.pegasus_key,
+                           d  = tmp,
+                           p2 = args.colonial_pegasus).split()
+        subprocess.Popen(upd).communicate()
+        sys.stdout.write( "Down, Up: %s" % tmp)
+        shutil.rmtree(tmp)
+
+    sys.stdout.write("\n\n")
+  
+if __name__ == "__main__":
+    main()
